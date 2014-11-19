d719ca68cd1a8ce915788808327b0ae08f1134c6
Hannah Brock <brockplusplus@gmail.com>
2014-11-19 11:36:40 -0700

    Add directory listing to main API servlet (should be updated when new API calls are added). Add JSON to project dependenceies.

--- src/main/java/org/sportim/service/ServiceMain.java
+++ src/main/java/org/sportim/service/ServiceMain.java
@@ -1,22 +1,33 @@
 package org.sportim.service;
 
+import org.json.JSONObject;
+
 import javax.servlet.annotation.WebServlet;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Path;
 
 /**
- * Created by hannah on 11/18/14.
+ * Servlet displays call directory for the SportIM API.
+ *
+ * @author hbrock
  */
-// The Java class will be hosted at the URI path "/helloworld"
 @Path("/")
 @WebServlet
 public class ServiceMain {
+    public static final String VERSION = "0.1";
 
     @GET
-    @Produces("text/plain")
-    public String getClichedMessage() {
-        // TODO put API call directory here
-        return "SportIM - api";
+    @Produces("application/json")
+    public String getDirectory() {
+        JSONObject json = new JSONObject();
+        json.put("version", VERSION);
+
+        JSONObject directory = new JSONObject();
+        directory.put("users", "/sportim/rest/user");
+
+        json.put("directory", directory);
+
+        return json.toString();
     }
 }
